1f31d24f7aa4c51795f8242060b1e7c2bc5c2734
Christopher Cope <Shadow6363@gmail.com>
2015-12-05 13:34:38 -0500

    Complete 2nd star for Day 02 of Advent of Code.

--- 2015/Advent of Code/Day02.py
+++ 2015/Advent of Code/Day02.py
@@ -5,17 +5,20 @@ import sys
 
 def main():
     dimensions = []
-    sq_ft = 0
+    sq_ft_paper, ft_ribbon = 0, 0
 
     for line in sys.stdin:
         dimensions.append(sorted([int(dimension) for dimension in line.split('x')]))
 
     for dimension in dimensions:
-        sq_ft += 3 * (dimension[0] * dimension[1])
-        sq_ft += 2 * (dimension[0] * dimension[2])
-        sq_ft += 2 * (dimension[1] * dimension[2])
+        sq_ft_paper += 3 * (dimension[0] * dimension[1])
+        sq_ft_paper += 2 * (dimension[0] * dimension[2])
+        sq_ft_paper += 2 * (dimension[1] * dimension[2])
 
-    print sq_ft
+        ft_ribbon += (dimension[0] * dimension[1] * dimension[2])
+        ft_ribbon += (2 * dimension[0]) + (2 * dimension[1])
+
+    print ft_ribbon
 
 if __name__ == '__main__':
     main()
