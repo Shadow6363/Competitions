2aa3c3086ebeaf200ba49e0c5747db93474b5263
Christopher Cope <Shadow6363@gmail.com>
2015-12-06 00:32:27 -0500

    Complete 2nd star for Day 06 of Advent of Code.

--- 2015/Advent of Code/Day06.py
+++ 2015/Advent of Code/Day06.py
@@ -4,8 +4,8 @@ import sys
 
 
 def main():
-    grid = [[False for _ in xrange(1000)] for _ in xrange(1000)]
-    lit = 0
+    grid = [[0 for _ in xrange(1000)] for _ in xrange(1000)]
+    brightness = 0
 
     for line in sys.stdin:
         instruction, from_coords, _, dest_coords = line.strip().rsplit(' ', 3)
@@ -15,22 +15,17 @@ def main():
         for x_coord in xrange(from_coords[0], dest_coords[0] + 1):
             for y_coord in xrange(from_coords[1], dest_coords[1] + 1):
                 if instruction == 'turn on':
-                    if not grid[x_coord][y_coord]:
-                        grid[x_coord][y_coord] = True
-                        lit += 1
+                    brightness += 1
+                    grid[x_coord][y_coord] += 1
                 elif instruction == 'turn off':
-                    if grid[x_coord][y_coord]:
-                        grid[x_coord][y_coord] = False
-                        lit -= 1
+                    if grid[x_coord][y_coord] > 0:
+                        brightness -= 1
+                        grid[x_coord][y_coord] -= 1
                 else:
-                    if grid[x_coord][y_coord]:
-                        grid[x_coord][y_coord] = False
-                        lit -= 1
-                    else:
-                        grid[x_coord][y_coord] = True
-                        lit += 1
+                    brightness += 2
+                    grid[x_coord][y_coord] += 2
 
-    print lit
+    print brightness
 
 if __name__ == '__main__':
     main()
