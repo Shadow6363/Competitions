38256cda5d272cbedb243f07a94bbf654b5eac79
Christopher Cope <Shadow6363@gmail.com>
2015-12-05 19:18:53 -0500

    Complete 2nd star for Day 05 of Advent of Code.

--- 2015/Advent of Code/Day05.py
+++ 2015/Advent of Code/Day05.py
@@ -5,14 +5,12 @@ import sys
 
 
 def main():
-    vowel_regex = re.compile(r'[aeiou][^aeiou]*[aeiou][^aeiou]*[aeiou]')
-    dup_regex = re.compile(r'([a-z])\1')
+    pair_two_regex = re.compile(r'([a-z][a-z])[a-z]*\1')
+    dup_between_regex = re.compile(r'([a-z])[a-z]\1')
     nice = 0
 
     for line in sys.stdin:
-        if 'ab' in line or 'cd' in line or 'pq' in line or 'xy' in line:
-            continue
-        if vowel_regex.search(line) and dup_regex.search(line):
+        if pair_two_regex.search(line) and dup_between_regex.search(line):
             nice += 1
 
     print nice
