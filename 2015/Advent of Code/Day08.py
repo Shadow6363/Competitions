ce1e25a34f2111aaf72c366bc93134ce7208b4b9
Christopher Cope <Shadow6363@gmail.com>
2015-12-09 18:25:10 -0500

    Complete 2nd star for Day 08 of Advent of Code.

--- 2015/Advent of Code/Day08.py
+++ 2015/Advent of Code/Day08.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    num_char_code, num_char_mem = 0, 0
+    num_char_code, num_char_mem, num_char_enc = 0, 0, 0
 
     for line in sys.stdin:
         line = line.strip()
@@ -14,7 +14,9 @@ def main():
         eval_line = ast.literal_eval(line)
         num_char_mem += len(eval_line)
 
-    print num_char_code - num_char_mem
+        num_char_enc += (2 + len(line) + line.count('\\') + line.count('"'))
+
+    print num_char_enc - num_char_code
 
 if __name__ == '__main__':
     main()
