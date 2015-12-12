a921a7abe02847e2443cbfbf99bc0b6149547a2a
Christopher Cope <Shadow6363@gmail.com>
2015-12-12 13:12:05 -0500

    Complete 1st star for Day 10 of Advent of Code.

---
+++ 2015/Advent of Code/Day10.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import re
+import sys
+
+
+def main():
+    start_num = sys.stdin.readline().strip()
+
+    num_regex = re.compile(r'([0-9])\1*')
+
+    for _ in xrange(40):
+        result = []
+        index = 0
+        match = num_regex.search(start_num, index)
+
+        while match:
+            result.append('{0}{1}'.format(match.end() - index, match.group(0)[0]))
+            index = match.end()
+            match = num_regex.search(start_num, index)
+
+        start_num = ''.join(result)
+
+    print len(start_num)
+
+
+if __name__ == '__main__':
+    main()
