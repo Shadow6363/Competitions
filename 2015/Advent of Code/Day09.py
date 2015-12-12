aee24cbaf4da957d204f57477ad3a39960bf88da
Christopher Cope <Shadow6363@gmail.com>
2015-12-12 13:10:42 -0500

    Complete 1st star for Day 09 of Advent of Code.

---
+++ 2015/Advent of Code/Day09.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from collections import defaultdict
+
+import itertools
+import sys
+
+
+distances = defaultdict(dict)
+
+def tsp(start):
+    costs = {}
+    costs[(frozenset((start,)), start)] = 0
+
+    for length in xrange(2, len(distances.keys()) + 1):
+        combos = itertools.combinations(distances[start].keys(), length - 1)
+        for vertices in combos:
+            vertices_start = frozenset(vertices) | {start}
+
+            costs[(vertices_start, start)] = float('inf')
+
+            for from_loc in vertices:
+                costs[(vertices_start, from_loc)] = min(
+                    costs[(vertices_start - {from_loc}, dest_loc)] +
+                    distances[from_loc][dest_loc]
+                    for dest_loc in vertices_start - {from_loc}
+                )
+
+    return min(
+        costs[(frozenset(distances[start].keys()) | {start}, vertex)]
+        for vertex in distances[start].keys()
+    )
+
+def main():
+    for line in sys.stdin:
+        line = line.strip()
+        path, distance = line.split(' = ')
+        from_loc, dest_loc = path.split(' to ')
+
+        distances[from_loc][dest_loc] = int(distance)
+        distances[dest_loc][from_loc] = int(distance)
+
+    print min(tsp(start) for start in distances.iterkeys())
+
+
+if __name__ == '__main__':
+    main()
